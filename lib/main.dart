f1bbe0d23ee2423aec32611652418a90fdccc7af
kosala504 <kosala504@gmail.com>
2023-11-23 22:32:01 +0530

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,38 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+//import 'package:my_first_app/auth/auth.dart';
+import 'package:my_first_app/firebase_options.dart';
 //import 'package:my_first_app/pages/login_page.dart';
 import 'package:my_first_app/pages/home_page.dart';
 //import 'package:my_first_app/pages/profile.dart';
 import 'package:my_first_app/styles/app_colors.dart';
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget 
-{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) 
-  {
+  Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
-    return MaterialApp
-    (
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           fontFamily: 'Urbanist',
           scaffoldBackgroundColor: AppColors.background),
-          home: const HomePage(),
+      home: const HomePage(),
       //initialRoute: '/',
 
-      routes:
-       {
-      //  '/': (context) => LoginPage(),
+      routes: {
+        //  '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
-       // '/profile':(context) => const Profile(),
-        },
+        // '/profile':(context) => const Profile(),
+      },
     );
   }
 }
