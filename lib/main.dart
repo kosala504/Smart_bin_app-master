1f92d83028de517bb4140bcd744167b83562edc0
Bimali_Fernando <=>
2023-12-13 20:15:48 +0530

    profile change

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 //import 'package:my_first_app/auth/auth.dart';
 import 'package:my_first_app/firebase_options.dart';
@@ -9,9 +9,7 @@ import 'package:my_first_app/styles/app_colors.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform); //firebase
-
+ //await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
